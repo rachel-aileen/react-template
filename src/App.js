9e3fb9c529e018831be79afa318a5948890fbaeb
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-01-15 15:42:46 -0800

    CORRECT TEMPLATE CLICK THIS FOR REACT TEMPLATE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 
 import './App.css';
-import Hero from './layouts/Hero';
-import Navbar from './layouts/Navbar';
-import Notification from './layouts/Notification';
-import TrendingProducts from './layouts/TrendingProducts';
+
 
 function App() {
   return (
     <div>
 
-      <Notification />
-
-      <Navbar />
-
-      <Hero />
-
-      <TrendingProducts />
-
-      <TrendingProducts />
 
     </div>
   );
--- src/layouts/Notification.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Notification() {
-    return (
-        <div className='notification'>
-            Free Shipping on Domestic Orders Over $75
-        </div>
-    )
-}
